51e688074b84a7eb41b3f7f09d0048386dd0243f
Juanillo22-hue <137093381+Juanillo22-hue@users.noreply.github.com>
2023-07-02 19:43:40 -0500

    Delete Dinidiseños directory

--- Dinidiseños/administrador/cruds/crud_gestion_personal/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../../dbConnection.php");
-
-$id = $_GET['id'];
-
-$result = mysqli_query($mysqli, "DELETE FROM users WHERE id = $id");
-
-header("Location:../../dahsboard.php");
